97c3b85a2f222e7df0882d4239c3e3269b1c77b2
homieproject <57488501+homieproject@users.noreply.github.com>
2019-11-20 14:47:49 +0530

    updated the firebase changes

--- controls/controls.js
+++ controls/controls.js
@@ -14,10 +14,36 @@ for (i = 0; i < coll.length; i++) {
 }
 
 var apl = document.getElementsByClassName("appliance");
-for(i = 0; i < apl.length; i++)
-{
-    apl[i].addEventListener("click",function(){
+
+apl[0].addEventListener("click",function(){
+    var database = firebase.database().ref();
+    var data = {
+        state : document.getElementById("switch1").checked
+    };
+    
+    database.child("Room1").child("Appliance-1").set(data);
         this.classList.toggle("aftercollapse");
         this.classList.toggle("appliance");
     });
-}
+
+apl[1].addEventListener("click",function(){
+    var database = firebase.database().ref();
+    var data = {
+        state : document.getElementById("switch2").checked
+    };
+    
+    database.child("Room1").child("Appliance-2").set(data);
+        this.classList.toggle("aftercollapse");
+        this.classList.toggle("appliance");
+    });
+
+apl[2].addEventListener("click",function(){
+    var database = firebase.database().ref();
+    var data = {
+        state : document.getElementById("switch3").checked
+    };
+    
+    database.child("Room1").child("Appliance-3").set(data);
+        this.classList.toggle("aftercollapse");
+        this.classList.toggle("appliance");
+    });
