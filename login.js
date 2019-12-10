14220da63b8d801be0fa4ec2bbc8969e6e0efade
homieproject <57488501+homieproject@users.noreply.github.com>
2019-12-10 17:34:31 +0530

    added a commented login code

--- login.js
+++ login.js
@@ -1,3 +1,27 @@
+// code for login
+
+// var log = document.getElementsByClassName("login-btn");
+// var uname = document.getElementById("myTxt");
+// var pwd = document.getElementById("myPwd");
+// var login = firebase.database().ref("Login/");
+// log[0].addEventListener("click", function () {
+// 	login.child(uname.value).child("pwd").on("value", function(snapshot){
+//     passjson = snapshot;
+//     console.log(passjson.val());
+// }, function(errorObject){
+//     console.log("Unable to read data : " + errorObject);
+// });
+// 		if(pwd.value == passjson.val())
+//         {
+// 			console.log("correct Pwd..Logging in!");
+//         }
+// 		else
+//         {
+// 			console.log("Incorrect Pwd.. Try Again!");
+//         }
+// //     window.location.href = "main/main.html";
+// });
+
 var log = document.getElementsByClassName("login-btn");
 
 log[0].addEventListener("click", function () {
